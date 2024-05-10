a20bc1191a828c6109d50292606fa637ae8ce99e
Lucca Matteoni <la.245@hotmail.com>
2024-05-10 10:17:40 -0300

    Pequenas alterações para testar deploy no Vercel 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { WebChatContainer } from '@ibm-watson/assistant-web-chat-react';
 
 const webChatOptions = {
   integrationID: "d688eaa6-f93f-499c-932b-d2b64597eba5", // The ID of this integration.
-    region: "us-south", // The region your integration is hosted in.
+    region: "us-south" as "us-south", // The region your integration is hosted in.
     serviceInstanceID: "063a22be-fa71-4fa8-b1e7-1a55fb7a84d0", // The ID of your service instance.
     onLoad: async (instance: any) => { await instance.render(); }
 };
