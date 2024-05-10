c10eb25df50c5c5c240470d549012b3a391aafa9
Lucca Matteoni <la.245@hotmail.com>
2024-05-10 09:31:09 -0300

    Pequenas alterações para testar deploy no Vercel 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const webChatOptions = {
   integrationID: "d688eaa6-f93f-499c-932b-d2b64597eba5", // The ID of this integration.
     region: "us-south", // The region your integration is hosted in.
     serviceInstanceID: "063a22be-fa71-4fa8-b1e7-1a55fb7a84d0", // The ID of your service instance.
-    onLoad: async (instance) => { await instance.render(); }
+    onLoad: async (instance: any) => { await instance.render(); }
 };
 
 const poppins = Poppins({
